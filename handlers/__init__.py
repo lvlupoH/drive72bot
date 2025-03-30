2dc8f5445cd3cfe8f4e66ca8164493b013a762b2
lvlupoH <xayc72@gmail.com>
2025-03-30 11:41:00 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,4 @@
-from .categories import handle_categories, show_moto_packages, show_auto_packages
+from .categories import handle_categories, show_packages
 from .callbacks import setup_callbacks_handler
 from .gallery import handle_photo, show_gallery
 from .instructors import show_instructors
