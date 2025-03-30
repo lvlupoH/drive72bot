8910c14b028c5e206c4ee3def324f802c2efaf3f
lvlupoH <xayc72@gmail.com>
2025-03-30 12:50:13 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,6 +3,8 @@ from .callbacks import get_callback_handler  # Исправленный импо
 from .gallery import show_gallery
 from .instructors import show_instructors
 from .profile import show_profile
+# handlers/__init__.py
+from .admin import get_admin_handler  # Добавьте эту строку
 
 __all__ = [
     'handle_categories',
