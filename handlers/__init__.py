6164edb4071ae38696255760e58ecbd21d32122a
lvlupoH <xayc72@gmail.com>
2025-03-30 17:28:55 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,18 @@
-from .categories import handle_categories, show_packages
-from .callbacks import get_callback_handler  # Исправленный импорт
+from .categories import (
+    handle_categories,
+    show_moto_packages,
+    show_auto_packages,
+    show_package_details,
+    handle_back
+)
+from .callbacks import start_callback, get_name, get_phone, get_question
 from .back import back_handler
 from .gallery import show_gallery
 from .instructors import show_instructors
 from .profile import show_profile
 # handlers/__init__.py
 from .admin import get_admin_handler  # Добавьте эту строку
+from .admin import admin_panel
 
 __all__ = [
     'handle_categories',
