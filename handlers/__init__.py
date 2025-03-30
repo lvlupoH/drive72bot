c11657063705b1f3a95f87b4b952a883ce27c6f7
lvlupoH <xayc72@gmail.com>
2025-03-30 12:14:48 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,16 +1,13 @@
 from .categories import handle_categories, show_packages
-from .callbacks import setup_callbacks_handler
-from .gallery import handle_photo, show_gallery
+from .callbacks import get_callback_handler  # Исправленный импорт
+from .gallery import show_gallery
 from .instructors import show_instructors
-from .admin import admin_panel
+from .profile import show_profile
 
 __all__ = [
     'handle_categories',
-    'show_moto_packages',
-    'show_auto_packages',
-    'setup_callbacks_handler',
-    'handle_photo',
+    'get_callback_handler',
     'show_gallery',
     'show_instructors',
-    'admin_panel'
+    'show_profile'
 ]
