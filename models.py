f9db4ba4ad72a43e03b11f557fbe44bd4125b8bd
lvlupoH <xayc72@gmail.com>
2025-04-03 00:46:26 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,24 +1,21 @@
-# models.py
 from sqlalchemy import create_engine, Column, Integer, String, Date
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from config import Config
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'students'
     
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, unique=True)
-    full_name = Column(String)
-    group = Column(String)
+    user_id = Column(Integer)
+    full_name = Column(String(100))
+    group = Column(String(50))
     internal_exam = Column(Date)
     state_exam = Column(Date)
     practical_exam = Column(Date)
-    exam_address = Column(String)
-    notes = Column(String)
+    exam_address = Column(String(200))
+    notes = Column(String(500))
 
-# Инициализация базы данных
 engine = create_engine(Config.DATABASE_URL)
-Session = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
