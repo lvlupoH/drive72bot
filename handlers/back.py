f1f2213019c8ad2038691b76948131b55ebd1091
lvlupoH <xayc72@gmail.com>
2025-04-03 10:31:46 +0500

    Update back.py

--- handlers/back.py
+++ handlers/back.py
@@ -1,14 +1,15 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from handlers.categories import handle_categories
+from .categories import handle_categories
 
 async def back_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
+    
     if query.data == "back_main":
         keyboard = [
             [{"text": "Категории", "callback_data": "categories"}],
-            [{"text": "Личный кабинет", "callback_data": "profile"}]
+            [{"text": "Обратный звонок", "callback_data": "callback_request"}]
         ]
         await query.edit_message_text(
             "Главное меню:",
