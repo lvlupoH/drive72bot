01e50cccd6bfe70b073fb1375b4e2334bd21c80d
lvlupoH <xayc72@gmail.com>
2025-04-03 10:05:43 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,15 @@
-# handlers/__init__.py
-from .start import start
+from .admin import admin_conversation_handler
 from .callbacks import setup_callbacks_handler
-from .requests import setup_requests_handler
+from .categories import handle_categories, show_packages
+from .profile import profile_handler, check_profile
 from .back import back_handler
-from .admin import get_admin_handler
-from .profile import profile_handler
+
+__all__ = [
+    'admin_conversation_handler',
+    'setup_callbacks_handler',
+    'handle_categories',
+    'show_packages',
+    'profile_handler',
+    'check_profile',
+    'back_handler'
+]
