0a4754f8d47d7ac00ec5a3a511be54ed3e11b7eb
lvlupoH <xayc72@gmail.com>
2025-04-03 20:06:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 from dotenv import load_dotenv
 import sys
@@ -9,15 +8,17 @@ class Config:
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     DATABASE_URL = os.getenv("DATABASE_URL")
     ADMIN_ID = int(os.getenv("ADMIN_ID"))
-    
+    WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+    PORT = int(os.getenv("PORT", 10000))
+
     @classmethod
     def validate(cls):
         missing = []
-        for var in ["TELEGRAM_TOKEN", "DATABASE_URL", "ADMIN_ID"]:
+        for var in ["TELEGRAM_TOKEN", "DATABASE_URL", "ADMIN_ID", "WEBHOOK_URL"]:
             if not getattr(cls, var):
                 missing.append(var)
         if missing:
-            print(f"Отсутствуют переменные: {', '.join(missing)}")
+            print(f"Ошибка: Отсутствуют переменные: {', '.join(missing)}")
             sys.exit(1)
 
 Config.validate()
