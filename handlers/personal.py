c660f26a18836e27ac108b0a1a28e6baafec43e2
lvlupoH <xayc72@gmail.com>
2025-04-06 18:15:33 +0500

    Delete handlers/personal.py

--- handlers/personal.py
+++
@@ -1,7 +0,0 @@
-# handlers/personal.py
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def handle_personal_cabinet(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Обработчик личного кабинета"""
-    await update.message.reply_text("Добро пожаловать в личный кабинет!")
