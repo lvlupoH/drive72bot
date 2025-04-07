0b2913dece289ab104b2fb3b1a0230d139ca4973
lvlupoH <xayc72@gmail.com>
2025-04-07 12:33:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import (
 from config import Config
 from handlers.categories import handle_categories, show_packages
 from handlers.back import back_handler
-from handlers.callbacks import get_callback_handler
+from handlers.callbacks import get_callback_handler  # Исправленный импорт
 from handlers.admin import get_admin_handler
 from handlers.gallery import handle_gallery
 from handlers.instructors import handle_instructors
@@ -47,7 +47,7 @@ def main():
     
     # Регистрация обработчиков
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(get_callback_handler())
+    application.add_handler(get_callback_handler())  # Корректное использование
     application.add_handler(CallbackQueryHandler(handle_categories, pattern="^categories$"))
     application.add_handler(CallbackQueryHandler(show_packages, pattern="^(cat_a|cat_b)$"))
     application.add_handler(CallbackQueryHandler(back_handler, pattern="^back_"))
