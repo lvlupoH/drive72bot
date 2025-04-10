27a797b7f445ea5f684f1e307666fdc747ab53b4
lvlupoH <xayc72@gmail.com>
2025-04-10 16:50:50 +0500

    Create contacts.py

---
+++ handlers/contacts.py
@@ -0,0 +1,19 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes
+
+async def handle_contacts(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    
+    keyboard = [
+        [InlineKeyboardButton("📍 Филиал 1", url="https://2gis.ru/tyumen/geo/70000001019159851")],
+        [InlineKeyboardButton("📍 Филиал 2", url="https://2gis.ru/tyumen/geo/1830115629746447")],
+        [InlineKeyboardButton("☎️ Заказать звонок", callback_data="callback_request")],
+        [InlineKeyboardButton("🌐 Сайт", url="https://driveavto72.ru")],
+        [InlineKeyboardButton("🔙 Назад", callback_data="back_main")]
+    ]
+    
+    await query.edit_message_text(
+        text="🏢 Наши филиалы:\n",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
