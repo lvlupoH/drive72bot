d8edec944f6b50601cc019dd95dc5a497611e02e
lvlupoH <xayc72@gmail.com>
2025-04-10 19:20:24 +0500

    Update back.py

--- handlers/back.py
+++ handlers/back.py
@@ -6,22 +6,28 @@ async def back_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
     
     data = query.data.split('_')[1]
-    keyboard = []
     
+    # Главное меню
     if data == 'main':
         keyboard = [
             [InlineKeyboardButton("Категории", callback_data="categories")],
             [InlineKeyboardButton("Обратный звонок", callback_data="callback_request")],
-            [InlineKeyboardButton("Галерея", callback_data="gallery")]
+            [InlineKeyboardButton("Дополнительные занятия", callback_data="extra_classes")],
+            [InlineKeyboardButton("Адреса и контакты", callback_data="contacts")],
+            [InlineKeyboardButton("Галерея", callback_data="gallery")],
+            [InlineKeyboardButton("Личный кабинет", callback_data="profile")]
         ]
-        await query.edit_message_text("Главное меню:", reply_markup=InlineKeyboardMarkup(keyboard))
-        
+        await query.edit_message_text(
+            "🏎️ Главное меню:",
+            reply_markup=InlineKeyboardMarkup(keyboard)
+    
+    # Возврат к категориям
     elif data == 'categories':
         keyboard = [
             [InlineKeyboardButton("Категория А, А1", callback_data="cat_a")],
             [InlineKeyboardButton("Категория В", callback_data="cat_b")],
-            [InlineKeyboardButton("Назад", callback_data="back_main")]
+            [InlineKeyboardButton("🔙 Назад", callback_data="back_main")]
         ]
-        await query.edit_message_text("Выберите категорию:", reply_markup=InlineKeyboardMarkup(keyboard))
-    
-    return
+        await query.edit_message_text(
+            "Выберите категорию:",
+            reply_markup=InlineKeyboardMarkup(keyboard))
