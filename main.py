7bc4c2a2c2a96868112a08060016b7482a009ec1
lvlupoH <xayc72@gmail.com>
2025-04-11 10:50:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ from handlers import (
     gallery,
     contacts
 )
+from sqlalchemy import create_engine
+from models import Base
+
+engine = create_engine(Config.DATABASE_URL)
+Base.metadata.create_all(engine)
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
