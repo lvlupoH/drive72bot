57b3b60028d7c7a9b1388a184c4089065f5f9a72
lvlupoH <xayc72@gmail.com>
2025-04-14 13:12:54 +0500

    Update contacts.py

--- handlers/contacts.py
+++ handlers/contacts.py
@@ -6,12 +6,11 @@ async def handle_contacts(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
     
     keyboard = [
-        [InlineKeyboardButton("📍 Филиал 1", url="https://2gis.ru/tyumen/geo/70000001019159851")],
-        [InlineKeyboardButton("📍 Филиал 2", url="https://2gis.ru/tyumen/geo/1830115629746447")],
-        [InlineKeyboardButton("📍 Филиал 3", url="https://2gis.ru/tyumen/geo/70000001044540338")],
-        [InlineKeyboardButton("📍 Филиал 4", url="https://2gis.ru/tyumen/geo/1830115629789564")],
-        [InlineKeyboardButton("☎️ Заказать звонок", callback_data="contacts_callback")],
-        [InlineKeyboardButton("🌐 Сайт", url="https://driveavto72.ru")],
+        [InlineKeyboardButton("📍 рп. Боровский, ул. Набережная д.55, офис 105", url="https://2gis.ru/tyumen/geo/70000001019159851")],
+        [InlineKeyboardButton("📍 г. Тюмень, ул. Николая Гондатти д.7/2, офис 210", url="https://2gis.ru/tyumen/geo/1830115629746447")],
+        [InlineKeyboardButton("📍 г. Тюмень, ул. Малыгина д.14", url="https://2gis.ru/tyumen/geo/70000001044540338")],
+        [InlineKeyboardButton("📍 г. Тюмень, ул. Широтная д.193, корп.1", url="https://2gis.ru/tyumen/geo/1830115629789564")],
+        [InlineKeyboardButton("🌐 Сайт Автошколы Drive", url="https://driveavto72.ru")],
         [InlineKeyboardButton("🔙 Назад", callback_data="back_main")]
     ]
     
