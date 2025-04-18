3a3703daab715f1af2e7343b57f0139b19c7c9f7
lvlupoH <xayc72@gmail.com>
2025-04-18 15:28:17 +0500

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import sys
 
 load_dotenv()
 
@@ -13,18 +12,16 @@ class Config:
     ADMIN_ID = int(os.getenv("ADMIN_ID"))
     WEBHOOK_URL = os.getenv("WEBHOOK_URL")
     PORT = int(os.getenv("PORT", 10000))
-    SCHOOL_ADDRESS = os.getenv("SCHOOL_ADDRESS", """
-    рп. Боровский, ул. Набережная д.55, офис 105", 
-    г. Тюмень, ул. Николая Гондатти д.7/2, офис 210", 
-    г. Тюмень, ул. Малыгина д.14", 
-    г. Тюмень, ул. Широтная д.193, корп.1",
-    """)
+    SCHOOL_ADDRESS = os.getenv(
+        "SCHOOL_ADDRESS",
+        "рп. Боровский, ул. Набережная д.55, офис 105\nг. Тюмень, ул. Николая Гондатти д.7/2, офис 210"
+    )
 
     @classmethod
     def validate(cls):
         required = ["TELEGRAM_TOKEN", "DATABASE_URL", "EMAIL_USER", "EMAIL_PASSWORD", "ADMIN_EMAIL", "ADMIN_ID", "WEBHOOK_URL"]
         missing = [var for var in required if not getattr(cls, var)]
         if missing:
-            sys.exit(f"Ошибка: Отсутствуют переменные окружения: {', '.join(missing)}")
+            raise ValueError(f"Missing environment variables: {', '.join(missing)}")
 
 Config.validate()
