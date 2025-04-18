25a4d0ad792d6c40d29355aa152182ec0242d334
lvlupoH <xayc72@gmail.com>
2025-04-18 10:51:54 +0500

    Update contacts.py

--- handlers/contacts.py
+++ handlers/contacts.py
@@ -4,7 +4,6 @@ from telegram.ext import ContextTypes
 async def handle_contacts(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
-    
     keyboard = [
         [InlineKeyboardButton("📍 рп. Боровский, ул. Набережная д.55, офис 105", url="https://2gis.ru/tyumen/geo/70000001019159851")],
         [InlineKeyboardButton("📍 г. Тюмень, ул. Николая Гондатти д.7/2, офис 210", url="https://2gis.ru/tyumen/geo/1830115629746447")],
@@ -13,12 +12,9 @@ async def handle_contacts(update: Update, context: ContextTypes.DEFAULT_TYPE):
         [InlineKeyboardButton("🌐 Сайт Автошколы Drive", url="https://driveavto72.ru")],
         [InlineKeyboardButton("🔙 Назад", callback_data="back_main")]
     ]
-    
     await query.edit_message_text(
         text="🏢 Наши филиалы:",
-        reply_markup=InlineKeyboardMarkup(keyboard)
-    )
-    
+        reply_markup=InlineKeyboardMarkup(keyboard))
 
 
 
